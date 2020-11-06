e2692ab9da4955cab3c13412f72d469ab070d0c4
iwutopia <67038954+iwutopia@users.noreply.github.com>
2020-11-06 16:03:59 +0900

    first commit

---
+++ webpack-init/dist/main.js
@@ -0,0 +1,17 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+
+// CONCATENATED MODULE: ./src/util.js
+function sayHello(name) {
+    console.log("hello", name);
+  }
+// CONCATENATED MODULE: ./src/index.js
+;
+
+function myFunc() {
+  sayHello("mike");
+  console.log("myFunc");
+}
+myFunc();
+/******/ })()
+;
